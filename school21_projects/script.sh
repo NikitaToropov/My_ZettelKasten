ab90d0a08fb5b9b06476c31760dc5806a078cb06
nikitatoropov <nikkyniknikita@gmail.com>
2022-01-27 19:30:02 +0300

    Last Sync: 27-01-2022 19:30:02

--- school21_projects/script.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-Architerture=$(uname -a)
-CPU_ph=$(grep "physical id" /proc/cpuinfo | sort | uniq | wc -l)
-vCPU=$(grep "^processor" /proc/cpuinfo | wc -l)
-mem_total=$(free --mega | awk '$1 == "Mem" {print $2}')
-mem_used=$(free --mega | awk '$1 == "Mem" {print $3}')
-mem_ratio=${free | grep Mem | awk '{printf ("%.2f%", $3/$2 * 100.0)}'}
