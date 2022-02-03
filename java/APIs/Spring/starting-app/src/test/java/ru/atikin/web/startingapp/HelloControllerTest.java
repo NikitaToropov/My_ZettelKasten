8b88a0ffb9bcd81009b56cfd2f3a17351360c0c5
nikitatoropov <nikkyniknikita@gmail.com>
2022-02-03 23:00:02 +0300

    Last Sync: 03-02-2022 23:00:02

---
+++ java/APIs/Spring/starting-app/src/test/java/ru/atikin/web/startingapp/HelloControllerTest.java
@@ -0,0 +1,26 @@
+package ru.atikin.web.startingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void getHello() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(content)
+    }
+}
