84a8c30696c475ac57052238497827044653d943
nikitatoropov <nikkyniknikita@gmail.com>
2022-02-03 23:30:02 +0300

    Last Sync: 03-02-2022 23:30:02

---
+++ java/APIs/Spring/starting-app/src/test/java/ru/atikin/web/startingapp/HelloControllerIT.java
@@ -0,0 +1,21 @@
+package ru.atikin.web.startingapp;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class HelloControllerIT {
+
+    @Autowired
+    private TestRestTemplate template;
+
+    @Test
+    public void getHello() throws Exception {
+        ResponseEntity<String> response = template.getForEntity("/", String.class);
+        Assertions.assertEquals("Hello! Its me Johny!", response.getBody());
+    }
+}
--- java/APIs/Spring/starting-app/src/test/java/ru/atikin/web/startingapp/HelloControllerTest.java
+++ java/APIs/Spring/starting-app/src/test/java/ru/atikin/web/startingapp/HelloControllerTest.java
@@ -1,15 +1,17 @@
 package ru.atikin.web.startingapp;
 
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-@SpringBootTest
+//@SpringBootTest // Создается весь контекст для теста.
+@WebMvcTest // Создается только веб уровень.
 @AutoConfigureMockMvc
 public class HelloControllerTest {
 
@@ -21,6 +23,6 @@ public class HelloControllerTest {
         mvc.perform(MockMvcRequestBuilders.get("/")
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(content)
+                .andExpect(MockMvcResultMatchers.content().string(Matchers.equalTo("Hello! Its me Johny!")));
     }
 }
