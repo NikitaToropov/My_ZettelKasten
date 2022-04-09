b73c9b769565c20e4d6a5dda49340878666d008b
nikitatoropov <nikkyniknikita@gmail.com>
2022-04-10 02:00:02 +0300

    Last Sync: 10-04-2022 02:00:02

--- java/APIs/Spring/hello-world/src/main/java/ru/atikin/web/helloworld/controllers/HelloController.java
+++ java/APIs/Spring/hello-world/src/main/java/ru/atikin/web/helloworld/controllers/HelloController.java
@@ -1,2 +1,13 @@
-package ru.atikin.web.helloworld.controllers;public class HelloController {
+package ru.atikin.web.helloworld.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello_world")
+    public String hi() {
+        return "hello_world";
+    }
 }
